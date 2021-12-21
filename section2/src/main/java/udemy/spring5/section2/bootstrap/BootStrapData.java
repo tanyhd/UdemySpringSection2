37c97b3c0fe18e935e91db96fd2bd639da6151ac
tanyhd <darren243@hotmail.com>
2021-12-21 15:52:50 +0800

    17. Initializing Data with Spring

---
+++ section2/src/main/java/udemy/spring5/section2/bootstrap/BootStrapData.java
@@ -0,0 +1,46 @@
+package udemy.spring5.section2.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import udemy.spring5.section2.domain.Author;
+import udemy.spring5.section2.domain.Book;
+import udemy.spring5.section2.repositories.AuthorRepository;
+import udemy.spring5.section2.repositories.BookRepository;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Domain Driven Design", "123123");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "323232699");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+        System.out.println("Started in Bootstrap");
+        System.out.println("Number of Books: " + bookRepository.count());
+
+    }
+
+}
--- section2/src/main/java/udemy/spring5/section2/domain/Author.java
+++ section2/src/main/java/udemy/spring5/section2/domain/Author.java
@@ -1,5 +1,6 @@
 package udemy.spring5.section2.domain;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.Entity;
@@ -19,15 +20,14 @@ public class Author {
     private String lastName;
 
     @ManyToMany(mappedBy = "authors")
-    private Set<Book> books;
+    private Set<Book> books = new HashSet<>();
 
     public Author() {
     }
 
-    public Author(String firstName, String lastName, Set<Book> books) {
+    public Author(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.books = books;
     }
 
     public Long getId() {
